7ebf516c07bd3bb1ca263178a908b507868113df
SmartSpace <smart.developer5555@gmail.com>
2022-05-14 19:45:53 +0600

    first commit

---
+++ src/constants/launchpad.js
@@ -0,0 +1,20 @@
+import { serializeTokens } from "./tokens";
+const serializedTokens = serializeTokens();
+
+const ifos = [
+  {
+    pid: 0,
+    tokenLabel: "BUSD",
+    token: serializedTokens.busd,
+    isActive: true,
+  },
+
+  {
+    pid: 1,
+    tokenLabel: "BUSD",
+    token: serializedTokens.busd,
+    isActive: true,
+  },
+];
+
+export default ifos;
